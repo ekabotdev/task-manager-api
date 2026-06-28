d1db3e1d195ad62af4288f6442241d0a1773121e
ekabotdev <ekabotdev@gmail.com>
2026-06-28 19:58:01 +0300

    Inital commit

---
+++ src/main/java/com/ekabotdev/taskmanagerapi/TaskmanagerapiApplication.java
@@ -0,0 +1,13 @@
+package com.ekabotdev.taskmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ekabotdev/taskmanagerapi/TaskmanagerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekabotdev.taskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
