1e2c781f3a980a3d081213e2afb21e4c24ba3fcd
ekabotdev <ekabotdev@gmail.com>
2026-06-28 23:23:02 +0300

    fix: fix exists by username method

--- src/main/java/com/ekabotdev/taskmanagerapi/repository/UserRepository.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.ekabotdev.taskmanagerapi.repository;
 
 
-import org.apache.catalina.User;
+
+import com.ekabotdev.taskmanagerapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-    User existsByUsername(String username);
+    boolean existsByUsername(String username);
 }
