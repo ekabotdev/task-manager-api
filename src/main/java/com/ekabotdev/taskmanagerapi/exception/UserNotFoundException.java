fa305b28619d09eb6667dc369e19da49750d279f
ekabotdev <ekabotdev@gmail.com>
2026-06-28 23:16:58 +0300

    fix: delete user not found exception

--- src/main/java/com/ekabotdev/taskmanagerapi/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ekabotdev.taskmanagerapi.exception;
-
-import com.ekabotdev.taskmanagerapi.repository.UserRepository;
-
-public class UserNotFoundException  extends RuntimeException{
-    public UserNotFoundException(UserRepository userRepository) {
-        super("User not found");
-    }
-}
