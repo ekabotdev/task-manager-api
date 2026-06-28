7283caa438c4c5aa9fa8c27367b4bcc5eb5ced8b
ekabotdev <ekabotdev@gmail.com>
2026-06-28 23:25:07 +0300

    feat: add register endpoint

--- src/main/java/com/ekabotdev/taskmanagerapi/service/UserService.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/service/UserService.java
@@ -1,8 +1,34 @@
 package com.ekabotdev.taskmanagerapi.service;
 
 
+import com.ekabotdev.taskmanagerapi.dto.RegisterRequest;
+import com.ekabotdev.taskmanagerapi.dto.RegisterResponse;
+import com.ekabotdev.taskmanagerapi.exception.UsernameAlreadyExistsException;
+import com.ekabotdev.taskmanagerapi.repository.UserRepository;
+
+ import com.ekabotdev.taskmanagerapi.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public RegisterResponse register(RegisterRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new UsernameAlreadyExistsException("Username already exists"
+            );
+        }
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+
+        User userSaved = userRepository.save(user);
+        return new RegisterResponse(
+                "User registered successfully"
+        );
+    }
+
 }
