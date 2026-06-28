260215b8b78290f524ab1cf8387aaaf34292c8b6
ekabotdev <ekabotdev@gmail.com>
2026-06-28 23:28:21 +0300

    feat: add registration endpoint in user controller

---
+++ src/main/java/com/ekabotdev/taskmanagerapi/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.ekabotdev.taskmanagerapi.controller;
+
+
+import com.ekabotdev.taskmanagerapi.dto.RegisterRequest;
+import com.ekabotdev.taskmanagerapi.dto.RegisterResponse;
+import com.ekabotdev.taskmanagerapi.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping("/register")
+    public RegisterResponse registerUser(@RequestBody RegisterRequest registerRequest) {
+        return userService.register(registerRequest);
+    }
+}
