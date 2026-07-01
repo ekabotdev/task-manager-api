e8e524f986094c3de29001b235bec23f445ddfe9
ekabotdev <ekabotdev@gmail.com>
2026-07-01 15:45:05 +0300

    feat; login response dto class

--- src/main/java/com/ekabotdev/taskmanagerapi/dto/LoginResponse.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/dto/LoginResponse.java
@@ -1,4 +1,15 @@
 package com.ekabotdev.taskmanagerapi.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponse {
+    private String message;
 }
