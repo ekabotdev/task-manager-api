13d608a3caa68e5b371c208dc762c2e98899f9f3
ekabotdev <ekabotdev@gmail.com>
2026-07-01 22:19:05 +0300

    feat: login to security filter chain bean

--- src/main/java/com/ekabotdev/taskmanagerapi/config/SecurityConfig.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register")
+                        .requestMatchers("/auth/register", "/auth/login")
                         .permitAll()
                         .anyRequest()
                         .authenticated());
