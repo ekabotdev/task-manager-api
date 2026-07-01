2f5b40c17fad40d3e9cc97e829ab75c4df7faaba
ekabotdev <ekabotdev@gmail.com>
2026-07-01 22:17:31 +0300

    feat: login endpoint

--- src/main/java/com/ekabotdev/taskmanagerapi/controller/UserController.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.ekabotdev.taskmanagerapi.controller;
 
 
+import com.ekabotdev.taskmanagerapi.dto.LoginRequest;
+import com.ekabotdev.taskmanagerapi.dto.LoginResponse;
 import com.ekabotdev.taskmanagerapi.dto.RegisterRequest;
 import com.ekabotdev.taskmanagerapi.dto.RegisterResponse;
 import com.ekabotdev.taskmanagerapi.service.UserService;
@@ -19,5 +21,9 @@ public class UserController {
     @PostMapping("/register")
     public RegisterResponse registerUser(@RequestBody RegisterRequest registerRequest) {
         return userService.register(registerRequest);
+    }
+    @PostMapping("/login")
+    public LoginResponse loginUser(@RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest);
     }
 }
