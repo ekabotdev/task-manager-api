6cb0664915777154fe3ae4994bc6baf6d77f66ed
ekabotdev <ekabotdev@gmail.com>
2026-07-02 23:17:48 +0300

    feat: add vaildation to create task request fields

--- src/main/java/com/ekabotdev/taskmanagerapi/dto/CreateTaskRequest.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/dto/CreateTaskRequest.java
@@ -1,6 +1,7 @@
 package com.ekabotdev.taskmanagerapi.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,8 @@ import lombok.Setter;
 @Getter
 public class CreateTaskRequest {
 
+    @NotBlank
     private String title;
+    @NotBlank
     private String description;
 }
