740d2e36abd23f7916fdc217bf836f177ad635d4
ekabotdev <ekabotdev@gmail.com>
2026-07-02 23:01:40 +0300

    feat: add task repository class

--- src/main/java/com/ekabotdev/taskmanagerapi/repository/TaskRepository.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/repository/TaskRepository.java
@@ -1,4 +1,11 @@
 package com.ekabotdev.taskmanagerapi.repository;
 
-public class TaskRepository {
+
+import com.ekabotdev.taskmanagerapi.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository  extends JpaRepository<Task, Long> {
+
 }
