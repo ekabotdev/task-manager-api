c22445b2289b34227c3ca8f15552bbbea2f66a6e
ekabotdev <ekabotdev@gmail.com>
2026-07-02 23:19:58 +0300

    feat: add create task method to task controller

--- src/main/java/com/ekabotdev/taskmanagerapi/controller/TaskController.java
+++ src/main/java/com/ekabotdev/taskmanagerapi/controller/TaskController.java
@@ -1,4 +1,22 @@
 package com.ekabotdev.taskmanagerapi.controller;
 
+
+import com.ekabotdev.taskmanagerapi.dto.CreateTaskRequest;
+import com.ekabotdev.taskmanagerapi.dto.TaskResponse;
+import com.ekabotdev.taskmanagerapi.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
 public class TaskController {
+    public final TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    public TaskResponse create ( @Valid @RequestBody CreateTaskRequest request) {
+        return taskService.create(request);
+    }
 }
